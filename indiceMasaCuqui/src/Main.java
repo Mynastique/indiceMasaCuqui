3d12c210391fc1d866683c8a61773e58104bd03e
Mynastique <mina.cursos@gmail.com>
2025-01-28 01:21:19 +0100

    Cambios
    Cambios en los textos por consola eliminando el "ln" para que aparezcan en la misma línea

--- indiceMasaCuqui/src/Main.java
+++ indiceMasaCuqui/src/Main.java
@@ -9,24 +9,29 @@ public class Main {
         Scanner sc;
         sc = new Scanner(System.in);
         peso = sc.nextDouble();
-        System.err.println("Introduce tu altura en metros (Ej:1.68):");
-        altura = sc.nextDouble();
-        imc = peso / (altura * altura);
-        if (imc < 18.5){
-            System.out.println("Estás riquísima");
-        } else if (imc > 18.5 && imc < 25){
-            System.out.println("Estás divina");
-        } else if (imc > 25 && imc < 30) {
-            System.out.println("Estás preciosa");
-        } else if (imc > 30 && imc < 35){
-            System.out.println("Diosa del olimpo");
-        } else if (imc > 35 && imc < 40){
-            System.out.println("Eres Imparable");
-        } else {
-            System.out.println("Estás Perrísima");
+        if (peso <= 0){
+            System.err.println("Los datos no son válidos");
         }
 
-
-
+        System.err.println("Introduce tu altura en metros (Ej:1,68):");
+        altura = sc.nextDouble();
+        if (altura <= 0){
+            System.err.println("Los datos no son válidos");
+        }
+        imc = peso / (altura * altura);
+        System.out.print("Tu Índice de Masa Cuqui es ");
+        if (imc < 18.5){
+            System.out.print("Bellísima");
+        } else if (imc > 18.5 && imc < 25){
+            System.out.print("Divina");
+        } else if (imc > 25 && imc < 30) {
+            System.out.print("Preciosa");
+        } else if (imc > 30 && imc < 35){
+            System.out.print("Diosa del olimpo");
+        } else if (imc > 35 && imc < 40){
+            System.out.print("Imparable");
+        } else {
+            System.out.print("Perrísima");
+        }
     }
 }
